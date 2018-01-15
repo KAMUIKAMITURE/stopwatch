04488104e8838d599fb0f07467a0d20f0c4813a2
KAMUIKAMITURE <tab.git.kamui@gmail.com>
2018-01-15 21:32:20 +0900

    commit

--- app/src/main/java/com/kamiture/kamui/stopwatch/Main2Activity.java
+++ app/src/main/java/com/kamiture/kamui/stopwatch/Main2Activity.java
@@ -3,9 +3,15 @@ package com.kamiture.kamui.stopwatch;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+import java.util.ArrayList;
 
 public class Main2Activity extends AppCompatActivity {
     private SharedPreferences data;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +20,11 @@ public class Main2Activity extends AppCompatActivity {
 
         data = getSharedPreferences("CountResult", MODE_PRIVATE);
         long elapsedTime = data.getLong("CountLong", 0);
+
+        ArrayList data = new ArrayList<>();
+        //data.add(elapsedTime);
+        //ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+
+       // listView.setAdapter(adapter);
     }
 }
